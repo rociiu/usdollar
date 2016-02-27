d5ae3ee815f26d382ae266890407508ac69c41e7
Roc Yu <rociiu.yu@gmail.com>
2016-02-27 09:42:04 +0800

    Initial Commit

---
+++ usdolloar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  usdolloar
+//
+//  Created by Roc Yu on 16/2/27.
+//  Copyright © 2016年 Roc Yu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
