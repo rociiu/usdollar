b0860a6233cd068453fb2d2f26c1769d2fdd006f
Roc Yu <rociiu.yu@gmail.com>
2016-02-28 12:22:43 +0800

    refactor duplicate code

--- usdolloar/CurrencyStore.swift
+++ usdolloar/CurrencyStore.swift
@@ -24,32 +24,8 @@ class CurrencyStore {
     var pastWeekCurrencies: [NSDate: Double] = [NSDate: Double]()
     
     func fetchLatestCurrency(completeHanlder: Double -> Void) {
-        let urlString = "https://api.fixer.io/latest?base=USD"
-        
-        if let url = NSURL(string: urlString) {
-            let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
-            
-            let dataTask = session.dataTaskWithURL(url) {
-                (data: NSData?, response: NSURLResponse?, error: NSError?) in
-                if data == nil {
-                    print("dont have data")
-                } else {
-                    var json: [String: AnyObject]
-                    do {
-                        json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions()) as! [String: AnyObject]
-                        let rates = json["rates"] as! [String: AnyObject]
-                        let currency = rates["CNY"] as! Double
-                        print(currency)
-                        completeHanlder(currency)
-                    } catch {
-                        print("error")
-                    }
-                }
-            }
-            
-            dataTask.resume()
-        }
-        
+        let date = NSDate()
+        self.fetchCurrencyWithDate(date, completeHandler: completeHanlder)
     }
 
     func fetchCurrencyWithDate(date: NSDate, completeHandler: Double -> Void) {
